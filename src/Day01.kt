c63617aed94ec9a01888757651765fcf383782d9
Florian Mueller <mail@florianm.de>
2024-12-03 13:03:56 +0100

    - ✨ Day 3 finished

--- src/Day01.kt
+++ src/Day01.kt
@@ -58,11 +58,11 @@ fun main() {
     check(part1(listOf("9   3")) == 6)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputLines("Day01_test")
     check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInputLines("Day01")
     "Day01 Part 1 ----".println()
     part1(input).println()
 
---
+++ src/Day03.kt
@@ -0,0 +1,40 @@
+fun main() {
+    fun part1(input: String): Int {
+        val regex = Regex("mul\\(([0-9]+),([0-9]+)\\)")
+
+        var result = 0
+        regex.findAll(input).forEach {
+            val (a, b) = it.destructured
+            result += a.toInt() * b.toInt()
+        }
+
+        return result
+    }
+
+    fun part2(input: String): Int {
+        val regex = Regex("do\\(\\)|don't\\(\\)|mul\\(([0-9]+),([0-9]+)\\)")
+
+        var result = 0
+        var shouldDo = true
+        regex.findAll(input).forEach {
+            if("do()" == it.value){
+                shouldDo = true
+            } else if("don't()" == it.value){
+                shouldDo = false
+            } else if(shouldDo) {
+                val (a, b) = it.destructured
+                result += a.toInt() * b.toInt()
+            }
+        }
+
+        return result
+    }
+
+    check(part1("mul(5,5)") == 25)
+    check(part1("mul ( 2 , 4 )") == 0)
+    check(part1("mul(6,9!") == 0)
+
+    val input = readInputString("Day03")
+    part1(input).println()
+    part2(input).println()
+}
